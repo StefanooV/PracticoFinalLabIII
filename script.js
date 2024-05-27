d04594af2d8041a29956d36245794ccd76b9b1be
StefanooV <99687887+StefanooV@users.noreply.github.com>
2024-05-27 17:02:47 -0300

    fiveChanges

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
 const apiUrl = "https://api.yumserver.com/16695/products";
 
-document.getElementById('formAgregar').addEventListener('submit', function(event){
-    event.preventDefault();
+async function getStock () {
+    try {
+        const response = await fetch(apiUrl);
+        const vehiculos = await response.json();
 
-    let modelo = document.getElementById('modelo').value;
-    let anio = document.getElementById('anio').value;
-    let pesos = document.getElementById('pesos').value;
-    let dolar = document.getElementById('dolar').value;
-    let imagen = document.getElementById('imagen').files[0];
+        console.log(vehiculos);
 
-    let formData = new formData();
-    formData.append('modelo', modelo);
-    formData.append('anio', anio);    
-    formData.append('pesos', pesos);    
-    formData.append('dolar', dolar);    
-    formData.append('imagen', imagen);
-    
+    } catch (error) {
+        console.error(error);
+    }
+}
+getStock();
+
+function MostrarProductos (productos) {
+    let html = ``;
+    for (let i = 0; i < productos.length; i++) {
+        let mostrarProductos = productos[i];
+        html += `
+            <div class= "productos-container">               
+                <h2>Modelo: ${mostrarProductos[i].modelo}</h2>
+                <span>Año de Fabricacion ${mostrarProductos[i].anio}</span>
+                <span>Precio pesos: ${mostrarProductos[i].precioPesos} </span>
+                <span>Precio Dolar: ${mostrarProductos[i].precioDolar}</span>
+            </div>
+        `
+    }
+
+    document.body.innerHTML = html
+
+}
+
+
+
+function CrearNuevoProducto(){
+    let producto = {
+        modelo: document.getElementById('modelo').value,
+        anio: document.getElementById('anio').value,
+        precioPesos: document.getElementById('precioPesos').value,
+        precioDolar: document.getElementById('precioDolar').value,
+    };
     fetch(apiUrl, {
         method: 'POST',
-        body: formData
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(producto)
     })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('resultado').innerText = 'Datos cargados exitosamente';
-        console.log('Success:', data);
-    })
-    .catch(error => {
-        document.getElementById('resultado').innerText = 'Error al cargar los datos';
-        console.error('Error:', error);
-    })
-})
+    .then(response => response.text())
+    .then( function (data) { 
+            if (data.trim() == 'OK'){
+                alert('Se creo el producto con exito');
+            }
+            else {
+                alert(data);
+            }
+        }
+    )
+    .catch(error => console.error('Error: ', error));
+}
+
+
+
